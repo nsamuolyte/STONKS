a22cccb343a23605ee28c3a12cdcdbdd3b767361
nsamuolyte <nsamuolyte@icloud.com>
2025-11-09 17:48:00 +0200

    Initial commit

---
+++ src/main/java/com/example/stonks/model/STOCK.java
@@ -0,0 +1,4 @@
+package com.example.stonks.model;
+
+public class STOCK {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.stonks {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.stonks to javafx.fxml;
+    exports com.example.stonks;
+}
