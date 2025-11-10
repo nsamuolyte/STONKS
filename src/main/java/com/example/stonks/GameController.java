4622fd3511d06261c9ee0e0a6bc7c9a562e9c23e
nsamuolyte <nsamuolyte@icloud.com>
2025-11-10 02:18:54 +0200

    Veikia speed pasirinkimas is combo box

--- src/main/java/com/example/stonks/GameController.java
+++ src/main/java/com/example/stonks/GameController.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.util.Duration;
 import javafx.scene.text.Text;
@@ -40,35 +41,51 @@ public class GameController {
     @FXML
     private NumberAxis yAxis;
 
+    @FXML
+    private ComboBox<String> speedCB;
+
     public int numb = 1; //greicio skaicius
     private XYChart.Series<Number, Number> series = new XYChart.Series<>();
     private STOCK stock = new STOCK("STONKS", 100.0);
     private int time = 0;
     private Timeline timeline;
 
-    public void initialize()
-    {
+    public void initialize() {
         String vardas = playerNameLabel.getText();
         player = new PLAYER(vardas, 1000.0);
 
         priceChart.getData().add(series);
         priceChart.setLegendVisible(false);
-        priceChart.getData().add(series);
 
-        // Atnaujinti kas 1 sekundę
-        timeline = new Timeline(new KeyFrame(Duration.seconds(numb), e -> updateChart())); // kitimo greitis
+        numb = 1;
+        timeline = new Timeline(new KeyFrame(Duration.seconds(numb), e -> updateChart()));
         timeline.setCycleCount(Timeline.INDEFINITE);
-        timeline.play();
-        updateChart();
+        timeline.play(); // paleidžiam animaciją
 
-        saskaitaTXT.textProperty().bind(
-                Bindings.format("%.2f €", player.balance)
-        );
+        saskaitaTXT.textProperty().bind(Bindings.format("%.2f €", player.balance));
+        akcijosTXT.textProperty().bind(Bindings.format("%d", player.ownedStocks));
 
-        akcijosTXT.textProperty().bind(
-                Bindings.format("%d", player.ownedStocks)
-        );
+        speedCB.getItems().addAll("Lėtai", "Normaliai", "Greitai");
+        speedCB.setValue("Greitai");
+
+        speedCB.setOnAction(e -> {
+            String pasirinkimas = speedCB.getValue();
+            switch (pasirinkimas) {
+                case "Lėtai" -> numb = 5;
+                case "Normaliai" -> numb = 2;
+                case "Greitai" -> numb = 1;
+            }
+
+            // Sustabdom seną grafiką
+            timeline.stop();
+
+            // Sukuriam naują su nauju greičiu
+            timeline = new Timeline(new KeyFrame(Duration.seconds(numb), ev -> updateChart()));
+            timeline.setCycleCount(Timeline.INDEFINITE);
+            timeline.play();
+        });
     }
+
 
     private void updateChart() {
         stock.updatePrice(); // atnaujina modelio kainą
